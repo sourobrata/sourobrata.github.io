2722e103c121611ad21772e40a9b187ecf7909e3
Sourobrata Sarkar <sourobrata.s@uniqode.com>
2026-01-13 16:02:06 +0530

    Updated pixel

--- insights.js
+++ insights.js
@@ -1,7 +1,7 @@
 // Load and initialize Uniqode Insights SDK
 (function() {
     var script = document.createElement('script');
-    script.src = 'https://cdn.uniqode.com/insights/pixel.js';
+    script.src = 'https://q.cdn.uniqode.com/insights/staging/v1/latest/sdk.js';
     script.onload = function() {
       window.insights.init({
         clientKey: 'cl_fdfc0147-6dc7-4c9a-8727-dc430242bded'
