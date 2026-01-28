d8b5f98bb6ec660431eff47a76722f66bdd13916
Sourobrata Sarkar <sourobrata.s@uniqode.com>
2026-01-28 13:19:23 +0530

    Updated client key

--- insights.js
+++ insights.js
@@ -4,7 +4,7 @@
     script.src = 'https://q.cdn.uniqode.com/insights/staging/v1/latest/sdk.js';
     script.onload = function() {
       window.insights.init({
-        clientKey: 'cl_fdfc0147-6dc7-4c9a-8727-dc430242bded'
+        clientKey: 'cl_e6e03b8b-53b5-4d63-a427-9ebc20eef218'
       });
     };
     document.head.appendChild(script);
