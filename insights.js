627bef6580a96ffcfee71b1b4fc24116cdad5b27
Samarth Parnami <samarth.p@uniqode.com>
2026-01-28 22:01:55 +0530

    change script for website

--- insights.js
+++ insights.js
@@ -1,7 +1,7 @@
 // Load and initialize Uniqode Insights SDK
 (function() {
     var script = document.createElement('script');
-    script.src = 'https://cdn.uniqode.com/insights/staging/v1/latest/sdk.js';
+    script.src = 'https://cdn.uniqode.com/insights/production/v1/latest/sdk.js';
     script.onload = function() {
       window.insights.init({
         clientKey: 'cl_e6e03b8b-53b5-4d63-a427-9ebc20eef218'
