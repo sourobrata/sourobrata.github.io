e1ae8ce757720e9af5bcf9a0525851e9b51932fa
Sourobrata Sarkar <sourobrata.s@uniqode.com>
2026-04-26 09:47:21 +0530

    fixed staging sdk path

--- insights.js
+++ insights.js
@@ -1,7 +1,7 @@
 // Load and initialize Uniqode Insights SDK
 (function() {
     var script = document.createElement('script');
-    script.src = 'https://q.cdn.uniqode.com/insights/staging/v1/1.2.2-beta/sdk.js';
+    script.src = 'https://q.cdn.uniqode.com/insights/staging/v1/1.2.2/sdk.js';
     script.onload = function() {
       window.insights.init({
         clientKey: 'cl_729c1d2e-e58b-4b29-93e3-722e148e2f7f'
